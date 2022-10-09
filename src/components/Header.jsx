975472924478b5889cd19bfd5ca37215d4a6305d
Priyanshu Sharma <ps940016@gmail.com>
2022-10-09 08:52:12 +0530

    Refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,10 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <header className='
-            py-5 
-            px-2 
-            2xl:px-44 
-            xl:px-20
-            text-white 
-            bg-[#16213E]'>
-            <h1 className='font-extralight text-[1.7rem] ml-4 overflow-hidden'>WeatherNow</h1>
+        <header className='py-5 px-2 2xl:px-44 xl:px-20 text-white bg-[#16213E]'>
+            <a href='github.com/xtechilad/weather-now' target='_blank' rel='noreferrer'>
+                <h1 className='font-extralight text-[1.7rem] ml-4 overflow-hidden'>WeatherNow</h1>
+            </a>
         </header>
     );
 }
